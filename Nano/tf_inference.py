7c38a764384f647faf5390fb48618d3f11a4be93
tenderzada <277198552@qq.com>
2021-04-05 22:09:42 +0800

    update tf

--- Nano/tf_inference.py
+++ Nano/tf_inference.py
@@ -8,13 +8,13 @@ PATH_TO_CKPT = 'models/ssdlite_mobilenet_v2.pb'
 def load_model():
     detection_graph = tf.Graph()
     with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.compat.v1.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
             with detection_graph.as_default():
-                sess = tf.Session(graph=detection_graph)
+                sess = tf.compat.v1.Session(graph=detection_graph)
                 return sess, detection_graph
 
 
